37708b730c2f9f6f0a0d875a8feda9446e8f26a1
manoj gaikwad <gaikwadm853@gmail.com>
2021-12-05 18:11:16 +0530

    Added : delete multiple employees

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -12,6 +12,8 @@ function EmployeeList() {
     const [currentNameSort, setCurrentNameSort] = useState('default');
     const [currentSalarySort, setCurrentSalarySort] = useState('default');
 
+    const [selectedRows, setSelectedRows] = useState([]);
+
     const sortTypes = {
         name: {
             up: {
@@ -67,7 +69,7 @@ function EmployeeList() {
             else if (currentNameSort === 'default') nextSort = 'up';
 
             setCurrentNameSort(nextSort);
-            setCurrentSalarySort('default');            
+            setCurrentSalarySort('default');
 
         } else if (sortField === 'salary') {
             if (currentSalarySort === 'down') nextSort = 'default';
@@ -112,17 +114,94 @@ function EmployeeList() {
             })
     }
 
+    const seleteRows = (e) => {
+        var checkboxes = document.querySelectorAll('input[name=selectRow]');
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (!checkboxes[i].checked) {
+                document.getElementById("masterCheckBox").checked = false;
+            }
+        }
+
+        if (e.target.checked) {
+            selectedRows.push(parseInt(e.target.value))
+            setSelectedRows([...selectedRows])
+        }
+
+        if (!e.target.checked) {
+            for (var i = 0; i < selectedRows.length; i++) {
+                if (selectedRows[i] === parseInt(e.target.value)) {
+                    selectedRows.splice(i, 1);
+                    i--;
+                }
+            }
+        }
+        console.log(selectedRows);
+    }
+
+    const selectedAllRows = (e) => {
+        var checkboxes = document.querySelectorAll('input[name=selectRow]');
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            selectedRows.pop()
+            setSelectedRows([...selectedRows])
+        }
+
+        if (e.target.checked) {
+            employeesArr.forEach(emp => {
+                selectedRows.push(emp.id)
+                setSelectedRows([...selectedRows])
+            })
+            for (var i = 0; i < checkboxes.length; i++) checkboxes[i].checked = true;
+        } else {
+            for (var i = 0; i < checkboxes.length; i++) checkboxes[i].checked = false;
+            employeesArr.forEach(emp => {
+                selectedRows.pop()
+                setSelectedRows([...selectedRows])
+            })
+        }
+        console.log(selectedRows);
+    }
+
+    const deleteMultipleEmployee = () => {
+        if (selectedRows.length < 1) {
+            toast.error("Select Atleast 1 Row", {
+                position: "top-right"
+            })
+        } else {
+            axios.delete('http://localhost:8085/deleteEmployees', {
+                headers: {
+                    ids : selectedRows
+                }
+            })
+                .then(res => {
+                    getEmployees();
+                    toast.success('Deleted Employees Successfully!');
+                })
+                .catch(err => {
+                    toast.error("Something Went Wrong!")
+                    console.log(err);
+                })
+        }
+    }
+
     return (
         <>
             <div className="container border py-4 px-5 list_div">
                 <div className="d-flex justify-content-between align-items-center">
                     <h3>Employee List</h3>
-                    <Link to="/add" className="btn btn-info" >Add Employee</Link>
+                    <div>
+                        <button className="btn btn-danger mr-1" onClick={deleteMultipleEmployee} >Delete</button>
+                        <Link to="/add" className="btn btn-info" >Add Employee</Link>
+                    </div>
                 </div>
                 <hr />
                 <table className="table table-bordered table-hover table-striped text-center">
                     <thead className="header_bg text-white">
                         <tr>
+                            <th>
+                                <input type="checkbox" name="selectRow" id="masterCheckBox" onClick={selectedAllRows} />
+                            </th>
                             <th>ID</th>
                             <th>Name
                                 <button className="sort_btn" onClick={() => onSortChange('name')}>
@@ -145,6 +224,9 @@ function EmployeeList() {
                             employeesArr.length > 0 ?
                                 [...employeesArr].sort((sortOn === "name") ? sortTypes[sortOn][currentNameSort].fn : sortTypes[sortOn][currentSalarySort].fn).map((employee, index) => {
                                     return <tr key={employee.id}>
+                                        <td>
+                                            <input type="checkbox" name="selectRow" value={employee.id} onChange={(e) => seleteRows(e)} />
+                                        </td>
                                         <td>{employee.id}</td>
                                         <td className="text-left"> <img src={employee.profilePic} className="profile_img mr-2" /> {employee.name} </td>
                                         <td>{employee.gender}</td>
